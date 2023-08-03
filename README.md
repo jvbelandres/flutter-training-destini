c4ef05d73b4feff611c5b76ef30c7aacc966b01a
Christopher Tan <christopher.tan@lexmark.com>
2023-08-03 12:54:46 +0800

    starter code

---
+++ README.md
@@ -0,0 +1,6 @@
+# Destiny (Choose your own adventure app)
+
+In this challenge, you will be building a choose your own adventure game like Bandersnatch. In the process, you should apply your knowledge learned in flutter navigation and routing.
+
+
+<img align=left width=200 src='Destini.gif'/>
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Destiny',
+      home: DestiniApp(),
+    ),
+  );
+}
+
+class DestiniApp extends StatelessWidget {
+  const DestiniApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
